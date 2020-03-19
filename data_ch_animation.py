66f8772d82106c5be22fdb8c83017ef4f22717eb
flobrec <58743125+flobrec@users.noreply.github.com>
2020-03-19 18:35:08 +0100

    charts per 100k, corrected issue in mapbox

--- data_ch_animation.py
+++ data_ch_animation.py
@@ -27,7 +27,8 @@ df_cant_abv = pd.read_csv("https://raw.githubusercontent.com/flobrec/covid19/mas
 #df_cant_abv = pd.read_csv("C:/Users/all/Downloads/Cantons_ABV.csv", sep=";")
 for i in range(0,26):
     cantons['features'][i]['id'] = df_cant_abv.iloc[i]['Regionsabkürzung']
-        
+
+df_demographic = pd.read_csv("https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/demographics.csv", sep=',', error_bad_lines=False)      
 
 df_orig = pd.read_csv("https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_switzerland.csv", sep=',', index_col='Date', error_bad_lines=False)
 
@@ -36,7 +37,11 @@ df = df_orig.stack().reset_index().rename(columns={'level_0':'Date','level_1':'C
 
 df_ch = df[df['Canton']=='CH']
 df_cantons = df[df['Canton']!='CH']
+df_cantons = pd.merge(df_cantons, df_demographic[['Canton','Population']], on='Canton', how='left')
+df_cantons['CasesPer100k'] = df_cantons['Cases'] / df_cantons['Population'] * 100000
 
+#bar charts
+#total cases
 bar_data = df_cantons.groupby(['Canton', 'Date'])['Cases'].sum().reset_index().sort_values('Date', ascending=True)
 
 fig = px.bar(bar_data, x="Date", y="Cases",
@@ -52,15 +57,48 @@ fig = px.bar(bar_data, x="Date", y="Cases",
 fig.update_xaxes(tickangle=-90, showticklabels=True, type = 'category')
 fig.show(renderer="browser")
 
+# cases per 100k
+bar_data_pc = df_cantons.groupby(['Canton', 'Date'])['CasesPer100k'].sum().reset_index().sort_values('Date', ascending=True)
+
+fig = px.bar(bar_data_pc, x="Date", y="CasesPer100k",
+             color='Canton', text = 'CasesPer100k',
+             orientation='v',
+             height=600,
+             title="Cases for Cantons per 100'000 inhabitants",
+             template="plotly_dark",
+             color_discrete_sequence= px.colors.cyclical.IceFire
+             )
+
+#fig.update_yaxes(type="log")
+fig.update_xaxes(tickangle=-90, showticklabels=True, type = 'category')
+fig.show(renderer="browser")
+
 # animation
+# total cases
 max_color = max(df_cantons['Cases'])
-fig = px.choropleth_mapbox(df, geojson=cantons, locations='Canton', color='Cases',
+fig = px.choropleth_mapbox(df_cantons, geojson=cantons, locations='Canton', color='Cases',
                            color_continuous_scale="sunsetdark",
                             range_color=(0, max_color),
                             mapbox_style="carto-darkmatter",
                             zoom=7.5, center = {"lat": 47.05048, "lon": 8.30635},
                             opacity=0.5,
                             labels={'Cases':'Confirmed cases'},
+                            animation_frame="Date",
+                           template="plotly_dark"
+                          )
+
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show(renderer="browser")
+
+# cases per 100k
+max_color = max(df_cantons['CasesPer100k'])
+fig = px.choropleth_mapbox(df_cantons, geojson=cantons, locations='Canton', color='CasesPer100k',
+                           color_continuous_scale="sunsetdark",
+                            range_color=(0, max_color),
+                            mapbox_style="carto-darkmatter",
+                            zoom=7.5, center = {"lat": 47.05048, "lon": 8.30635},
+                            opacity=0.5,
+                            labels={'CasesPer100k':"Confirmed cases per 100'000 inhabitants"},
                             animation_frame="Date",
                            template="plotly_dark"
                           )
